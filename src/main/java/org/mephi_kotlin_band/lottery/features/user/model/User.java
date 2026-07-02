50617d2a6e29f1f13481b0e75763e1f194b0fa35
Sovacote <160414655+Sovacote@users.noreply.github.com>
2026-07-02 11:59:57 +1000

    Create User.java

---
+++ src/main/java/org/mephi_kotlin_band/lottery/features/user/model/User.java
@@ -0,0 +1,37 @@
+package org.mephi_kotlin_band.lottery.features.user.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "password")
+public class User {
+
+    public enum Role {
+        ADMIN,
+        USER
+    }
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
